f5e6365237907f6562968ad6205603853af31e9b
Mateus Pontes <mateuspsdd@gmail.com>
2024-08-11 11:47:13 -0300

    refactor: `CliService`: Implement cli logic

--- src/core/CliService.js
+++ src/core/CliService.js
@@ -1,41 +1,68 @@
-import { checkbox } from "@inquirer/prompts";
-import JavaConstants from "../constants/JavaConstants.js";
+import { checkbox, select } from "@inquirer/prompts";
+import shell from "shelljs";
 import JavaCommand from "../command/JavaCommand.js";
-import PythonCommand from "../command/PythonCommand.js";
-import PythonConstants from "../constants/PythonConstants.js";
+import JavaConstants from "../constants/JavaConstants.js";
+import MavenInstaller from "../installers/build/MavenInstaller.js";
+import ChocolateyInstaller from "../installers/package_manager/ChocolateyInstaller.js";
+import GitInstaller from "../installers/versioning/GitInstaller.js";
 
 export default class CliService {
 
+	async installPackageManager() {
+		if (shell.which("choco") === null) {
+			new ChocolateyInstaller().install();
+		}
+	}
+
+	async versioningMenu() {
+		const result = await select({
+			message: "Do you want to install git?",
+			choices: [
+				{ name: "Yes", value: new GitInstaller() },
+				{ name: "No", value: null }
+			]
+		});
+
+		return result;
+	}
+	
 	async codeMenu() {
 		const result =  await checkbox({
 			message: "Choose a programming language",
 			choices: [
 				{ name: JavaConstants.NAME, value: new JavaCommand() },
-				{ name: PythonConstants.NAME, value: new PythonCommand() }
+				{ name: "None", value: null }
 			]
 		});
 
+		if (result === null) {
+			return;
+		}
+
 		const languagesConfigs = [];
 		for (let index = 0; index < result.length; index++) {
-			const languageData = await result[index].execute();
+ 			const languageData = await result[index].execute();
 			languagesConfigs.push(languageData);
 		}
 
 		return languagesConfigs;
 	}
 
-	async versioningMenu() {
-	}
-
-	async packageManagerMenu() {
-	}
-
 	async buildMenu() {
+		return await checkbox({
+			message: "Choose a build tool",
+			choices: [
+				{ name: "Maven", value: new MavenInstaller() },
+				{ name: "None", value: null }
+			]
+		});
 	}
 
 	async containerMenu() {
+		return null;
 	}
 	
 	async ideMenu() {
+		return null;
 	}
 }
