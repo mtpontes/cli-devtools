1f35513c45ea1cc8f70d4db76ebfa51d55d61408
Mateus Pontes <mateuspsdd@gmail.com>
2024-08-13 12:19:53 -0300

    feat: Add `PathResolver`

---
+++ src/util/PathResolver.js
@@ -0,0 +1,26 @@
+import chalk from "chalk";
+import fs from "fs";
+import path from "path";
+
+export default class PathResolver {
+
+	constructor() {}
+
+	static getInstallationPath(baseDir, keyword, toolName) {
+		const directories = fs.readdirSync(baseDir);
+		let completePath = null;
+		
+		for (const dir of directories) {
+			if (dir.includes(keyword)) {
+				completePath = path.join(baseDir, dir);
+				return completePath;
+			}
+		}
+
+		if (completePath === null) 
+			console.error(chalk.red(`
+				There was a problem setting the installation path in the PATH 
+				variable. Please manually configure your 
+				${chalk.bold(toolName)} tool`));
+	}
+}
