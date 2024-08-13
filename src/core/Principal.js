b1b52955f985437e36259d046b7b4b8f59ad3d61
Mateus Pontes <mateuspsdd@gmail.com>
2024-08-13 12:42:13 -0300

    refactor: `Principal`

--- src/core/Principal.js
+++ src/core/Principal.js
@@ -1,34 +1,38 @@
 import CliService from "./CliService.js";
 import Installer from "../installers/Installer.js";
+import LogManager from "../util/LogManager.js";
 
 export default class Principal {
 
-	constructor() {
-		this.service = new CliService();
-	}
+  constructor() {
+    this.service = new CliService();
+    this.logger = new LogManager();
+  }
 
-	async showMenu() {
-		let installers = [];
-		await this.service.installPackageManager();
-		const versioningInstaller = await this.service.versioningMenu();
-		const codeInstaller = await this.service.codeMenu();
-		const buildInstaller = await this.service.buildMenu();
-		const ideInstaller = await this.service.ideMenu();
-		const containerInstaller = await this.service.containerMenu();
+  async showMenu() {
+  	let installers = [];
+  	await this.service.installPackageManager();
+  	const versioningInstaller = await this.service.versioningMenu();
+  	const codeInstaller = await this.service.codeMenu();
+  	const buildInstaller = await this.service.buildMenu();
+  	const ideInstaller = await this.service.ideMenu();
+  	const containerInstaller = await this.service.containerMenu();
 
-		installers.push(
-			versioningInstaller,
-			codeInstaller,
-			buildInstaller,
-			ideInstaller,
-			containerInstaller
-		);
+  	installers.push(
+  		versioningInstaller,
+  		codeInstaller,
+  		buildInstaller,
+  		ideInstaller,
+  		containerInstaller
+  	);
 
-		installers = installers
-			.flat()
-			.filter(i => i instanceof Installer);
-		for (const installer of installers) {
-			await installer.install();
-		}
-	}
+  	installers = installers
+  		.flat()
+  		.filter(i => i instanceof Installer);
+
+  	const logData = [];
+  	for (const installer of installers) {
+  		logData.push(await installer.install());
+  	}
+  }
 }
