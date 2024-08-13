b57d941cc2dc5678e20fd46f7389252e331f2180
Mateus Pontes <mateuspsdd@gmail.com>
2024-08-13 12:38:34 -0300

    feat: Add `LogManager`

---
+++ src/util/LogManager.js
@@ -0,0 +1,54 @@
+import fs from "fs";
+
+export default class LogManager {
+
+  #filepath = "./logs/log.txt";
+
+  constructor() {}
+
+  getPath() {
+    return this.#filepath;
+  }
+
+  writeLog(newLog) {
+    let logData = this.#readFile();
+    logData = [... logData, ...newLog];
+    this.#writeLog(logData);
+  }
+
+  #readFile() {
+    this.#createIfNotExists();
+
+    let reading;
+    try {
+      reading = fs.readFileSync(this.#filepath, "utf-8");
+
+    } catch (error) {
+      reading = null;
+      console.error("An error occurred while reading log");
+    }
+
+    return reading !== null ? JSON.parse(reading) : [];
+  }
+
+  #writeLog(data) {
+
+    try {
+      fs.writeFileSync(this.#filepath, JSON.stringify(data, null, 2));
+
+    } catch (error) {
+      console.error("An error occurred while writing log");
+    }
+  }
+
+  #createIfNotExists() {
+    const path = this.#filepath.replace("/log.txt", "");
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path, { recursive: true });
+    }
+
+    if (!fs.existsSync(this.#filepath)) {
+      fs.writeFileSync(this.#filepath, "");
+    }
+  }
+}
