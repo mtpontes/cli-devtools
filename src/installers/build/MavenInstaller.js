a91bb2a89808c9fc48f56c5a432111d43cac4c79
Mateus Pontes <mateuspsdd@gmail.com>
2024-08-13 12:21:29 -0300

    feat: Add `MavenInstaller`

---
+++ src/installers/build/MavenInstaller.js
@@ -0,0 +1,34 @@
+import Installer from "../Installer.js";
+import shell from "shelljs";
+import PathResolver from "../../util/PathResolver.js";
+
+export default class MavenInstaller extends Installer {
+
+	#chocolateyName = "maven";
+	#path = PathResolver.getInstallationPath(
+		"C:\\ProgramData\\chocolatey\\lib\\maven", 
+		"apache-maven", 
+		"Maven"
+	);
+
+	constructor() {
+		super();
+	}
+
+	async install() {
+		await shell.exec(`choco install ${this.#chocolateyName} -y`);
+		
+		return this.#getToolOutput();
+	}
+
+	#getToolOutput() {
+		const version = this.#path.split("maven-")[1];
+
+		return {
+			name: "Maven",
+			version: version,
+			path: this.#path,
+			timestamp: new Date().toISOString()
+		};
+	}
+}
