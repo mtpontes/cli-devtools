7e5c002ccdf778ecbc980503756aafcb558d8245
Mateus Pontes <mateuspsdd@gmail.com>
2024-08-13 12:23:49 -0300

    refactor: `JavaConstants`

--- src/constants/JavaConstants.js
+++ src/constants/JavaConstants.js
@@ -5,20 +5,20 @@ export default class JavaConstants {
 	static JDK_8 = { 
 		name: "Eclipse Temurin 8",
 		winget_id: "EclipseAdoptium.Temurin.8.JDK",
-		maind_dir: "jdk-8",
+		main_dir: "jdk-8",
 		installation_path: "C:\\Program Files (x86)\\Eclipse Adoptium"
 	};
     
 	static JDK_11 = { 
 		name: "Eclipse Temurin 11",
 		winget_id: "EclipseAdoptium.Temurin.11.JDK",
-		maind_dir: "jdk-11", 
+		main_dir: "jdk-11", 
 		installation_path: "C:\\Program Files\\Eclipse Adoptium"
 	};
     
 	static JDK_17 = { 
 		name: "Eclipse Temurin 17",
-		maind_dir: "jdk-17", 
+		main_dir: "jdk-17", 
 		winget_id: "EclipseAdoptium.Temurin.17.JDK",
 		installation_path: "C:\\Program Files\\Eclipse Adoptium"
 	};
@@ -26,7 +26,7 @@ export default class JavaConstants {
 	static JDK_21 = { 
 		name: "Eclipse Temurin 21",
 		winget_id: "EclipseAdoptium.Temurin.21.JDK",
-		maind_dir: "jdk-21",
+		main_dir: "jdk-21",
 		installation_path: "C:\\Program Files\\Eclipse Adoptium"
 	};
 }
